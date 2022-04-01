4a3b3990ab79a70e317470200418be19b78e2277
Heo-jieun <65994153+Heo-jieun@users.noreply.github.com>
2022-04-01 22:20:42 +0900

    Create 02.신고결과받기.py

---
+++ 02.신고결과받기.py
@@ -0,0 +1,40 @@
+def solution(id_list, report, k):
+    
+    # dictionary로 관리
+    user_dic = {}
+    for user_name in id_list:
+        user_dic[user_name] = []
+    
+    for report_id in set(report) :
+        report_list = report_id.split()
+        user_dic[report_list[0]].append(report_list[1]) 
+        
+    # 신고당한 횟수 계산
+    reported_count = {}
+    for user_id in user_dic:
+        for reported_name in user_dic[user_id]:
+            if reported_name in reported_count:
+                reported_count[reported_name] += 1
+            else :
+                reported_count[reported_name] = 1
+    
+    # 정지 대상자 확정
+    stopper = []
+    for reported_name in reported_count :
+        if reported_count[reported_name] >= k:
+            stopper.append(reported_name)
+    
+    # 정지 문자 발송
+    maile_list = {}
+    for user_id in user_dic:
+        maile_list[user_id] = 0
+    
+    for user_id in user_dic:
+        for stop_id in stopper:
+            if stop_id in user_dic[user_id]:
+                maile_list[user_id] += 1
+    result = []
+    for maile in maile_list:
+        result.append(maile_list[maile])
+
+    return result
